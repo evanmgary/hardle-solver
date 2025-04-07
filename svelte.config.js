6c0e654a38d6fc79565ebe91ceb96ed86bf2f978
Evan Gary <evanmgary@gmail.com>
2025-04-07 14:34:34 -0500

    prep for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
+
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
@@ -11,7 +12,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+            base: process.env.NODE_ENV === 'production' ? '/hardle-solver' : '',
+        }
 	}
 };
 
